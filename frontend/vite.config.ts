3ffeefbb858ddb605cf1c1b49d70cb79723c6d68
Alexandre Henrique R Araujo <64115785+alexandre-henrique-rp@users.noreply.github.com>
2025-07-28 20:18:55 -0300

    Commit correção: coreção no dockerfile front end

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,4 +5,18 @@ import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
   plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
+  preview: {
+    port: 3000,
+    strictPort: true,
+  },
+  server: {
+    port: 3000,
+    strictPort: true,
+    host: true,
+    origin: "http://0.0.0.0:3000",
+  },
+  build: {
+    outDir: 'build',
+    emptyOutDir: true,
+  }
 });
