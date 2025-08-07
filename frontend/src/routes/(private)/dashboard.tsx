fb95c9465feea76b8ff813b9fbd04c800cb37f8f
Alexandre Henrique R Araujo <64115785+alexandre-henrique-rp@users.noreply.github.com>
2025-08-06 23:08:10 -0300

    ✨ feat: Adiciona componente Home ao Dashboard
    - Substitui o conteúdo anterior do dashboard pelo componente HomeComponent
    - Atualiza a versão do SW

--- frontend/src/routes/(private)/dashboard.tsx
+++ frontend/src/routes/(private)/dashboard.tsx
@@ -1,3 +1,4 @@
+import HomeComponent from '@/components/home-component'
 import { createFileRoute } from '@tanstack/react-router'
 
 export const Route = createFileRoute('/(private)/dashboard')({
@@ -5,11 +6,5 @@ export const Route = createFileRoute('/(private)/dashboard')({
 })
 
 function App() {
-  return (
-    <div className="flex-1 space-y-4 p-8 pt-6">
-      <div className="flex items-center justify-between space-y-2">
-        <h2 className="text-3xl font-bold tracking-tight">Dashboard</h2>
-      </div>
-    </div>
-  )
+  return <HomeComponent />
 }
