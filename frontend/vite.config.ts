103c015747ad4ef8c7891d47f7266a5579261158
Alexandre Henrique R Araujo <64115785+alexandre-henrique-rp@users.noreply.github.com>
2025-10-19 18:43:56 -0300

    🔨 chore: Altera porta do frontend de 3001 para 3000

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -67,7 +67,7 @@ export default defineConfig({
   },
   server: {
     host: '0.0.0.0',
-    port: 3001,
+    port: 3000,
     strictPort: true,
     open: false, // Desabilita abertura automática do navegador
     hmr: {
