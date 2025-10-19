f6b40181f4890bf53b1283e34ab95e550f2d17ba
Alexandre Henrique R Araujo <64115785+alexandre-henrique-rp@users.noreply.github.com>
2025-10-19 18:48:51 -0300

    🔨 chore: Altera porta do preview para 3000
    - Atualiza a porta do preview no arquivo de configuração do Vite

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -76,7 +76,7 @@ export default defineConfig({
   },
   preview: {
     host: '0.0.0.0',
-    port: 3001,
+    port: 3000,
     strictPort: true,
     allowedHosts: [
       'contas.kingdevtec.com',
